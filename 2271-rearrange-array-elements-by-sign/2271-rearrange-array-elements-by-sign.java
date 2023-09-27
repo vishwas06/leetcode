272dbad67527d1a9fb3a69f199e30a0f439d3a0d
vishwas06 <112758057+vishwas06@users.noreply.github.com>
2023-09-27 20:02:21 +0530

    Time: 3 ms (100%), Space: 84.6 MB (20.59%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,20 +1,31 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-    List<Integer> pos = new ArrayList<>();
-    List<Integer> neg = new ArrayList<>();
+    // List<Integer> pos = new ArrayList<>();
+    // List<Integer> neg = new ArrayList<>();
 
-    for(int i=0; i< nums.length; i++){
-        if(nums[i]>0){
-            pos.add(nums[i]);
-        }else{
-            neg.add(nums[i]);
+    // for(int i=0; i< nums.length; i++){
+    //     if(nums[i]>0){
+    //         pos.add(nums[i]);
+    //     }else{
+    //         neg.add(nums[i]);
+    //     }
+    // }
+
+    // for(int j=0; j<(nums.length/2); j++){
+    //         nums[2*j]=pos.get(j);
+    //         nums[2*j+1]=neg.get(j);
+    //         }
+    int[] arr= new int[nums.length];
+    int posIndex=0; int negIndex=1;
+    for(int i=0; i<nums.length; i++){
+            if(nums[i]<0){
+                    arr[negIndex] = nums[i];
+                    negIndex+=2;
+            }else{
+                arr[posIndex]=nums[i];
+            posIndex+=2;    
         }
     }
-
-    for(int j=0; j<(nums.length/2); j++){
-            nums[2*j]=pos.get(j);
-            nums[2*j+1]=neg.get(j);
-            }
-    return nums;
+    return arr;
     }
 }
