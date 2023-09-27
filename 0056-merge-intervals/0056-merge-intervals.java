3f40b7685d630ac8ab4a49c933592f35fc6aae1f
vishwas06 <112758057+vishwas06@users.noreply.github.com>
2023-09-27 22:03:53 +0530

    Time: 8 ms (92.08%), Space: 45.8 MB (26.87%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b)->a[0]-b[0]);
-     int n = intervals.length;
+        int n=intervals.length;
         for(int i=1;i<intervals.length;i++){
 		// checking  if the start of the current is <= end of previous
 		// merging the interval and setting the previous one as -1
@@ -9,8 +9,8 @@ class Solution {
                 intervals[i][1] = Math.max(intervals[i-1][1],intervals[i][1]);
                 intervals[i][0] = intervals[i-1][0];
                 intervals[i-1][0] = -1;
-                intervals[i-1][1] = -1;n--;
-            }
+                intervals[i-1][1] = -1;
+                n--;            }
         }
         
         int[][] ans = new int[n][2];
