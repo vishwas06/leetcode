aba071be548467011d7ccb7de39c801adc8cedfe
vishwas06 <112758057+vishwas06@users.noreply.github.com>
2023-10-05 23:00:27 +0530

    Time: 0 ms (100%), Space: 40.5 MB (93.46%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,45 +1,28 @@
 class Solution {
     public int search(int[] nums, int target) {
-      int point=0;
-        if(nums[0]<=nums[nums.length-1]) point =0;
-        else point=BSpoint(nums);
-        int l=point;
-        int h=nums.length-1;
-        int brr=BS(nums,l,h,target);
-        if(brr!=-1) return brr;
-        l=0;
-        h=point-1;
-        return BS(nums,l,h,target);
-    }
-    int BS(int[] nums,int l,int h,int target){
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(nums[mid]==target) return mid;
-            if(nums[mid]<target){
-                l=mid+1;
-            }
-            else{
-                h=mid-1;
-            }
+    int low=0;
+    int high=nums.length-1;
+
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[mid]==target){
+            return mid;
         }
-        return -1;
-    }
-    int BSpoint(int[] nums){
-        int l=0;
-        int h=nums.length-1;
-        int n=nums.length;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(nums[mid]>nums[n-1] && mid<(n-1) && nums[mid]>nums[mid+1]){
-                return mid+1;
+        if(nums[low]<=nums[mid]){
+            if(nums[low]<=target && target<=nums[mid]){
+                high=mid-1;
+            }else{
+                low=mid+1;    
             }
-            else if(nums[mid]>nums[n-1]){
-                l=mid+1;
+        }else{
+            if(nums[mid]<=target && target<=nums[high]){
+                low=mid+1;
+            }else {
+              high=mid-1;  
             }
-            else{
-                h=mid-1;
-            }
+
         }
-        return 0;
+    }
+    return -1;
     }
 }
