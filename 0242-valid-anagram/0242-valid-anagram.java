8f3c89e9e2b6881b0a97c5492ecff6ae658e9b8d
vishwas06 <112758057+vishwas06@users.noreply.github.com>
2023-10-07 22:20:41 +0530

    Time: 3 ms (90.52%), Space: 41.6 MB (97.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,19 +1,24 @@
 class Solution {
-    static String StrSort(String a){
-        char [] c = a.toCharArray();
-        Arrays.sort(c);
-        return new String(c);
-        }
+    
+     
     public boolean isAnagram(String s, String t) {
     
     if(s.length() != t.length()){
         return false;
     }
-       String s1 = StrSort(s);
-       String s2 = StrSort(t);
 
-       for(int i=0; i<=s1.length()-1; i++){
-           if(s1.charAt(i)!=s2.charAt(i)){
+    int[] res= new int[26];
+
+    for(int i=0; i<=s.length()-1; i++){
+        res[s.charAt(i)-'a']++;
+    }
+
+    for(int j=0; j<=t.length()-1; j++){
+         res[t.charAt(j)-'a']--;
+    }
+
+       for(int i=0; i<res.length; i++){
+           if(res[i]!=0){
                return false;
            }
        }
